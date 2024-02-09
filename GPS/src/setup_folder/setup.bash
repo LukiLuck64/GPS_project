d2e1804ef2c1f8d6743d15d2e511839beafc0b32
Luc Pizzuti <lucpizzuti2@gmail.com>
2024-02-09 09:24:22 +0100

    setup.bash modifié

--- GPS/src/setup_folder/setup.bash
+++ GPS/src/setup_folder/setup.bash
@@ -5,5 +5,5 @@ cd dev/
 sudo chmod 777 ttyUSB0 
 stty -F /dev/ttyUSB0 4800
 cd /home/luc/Documents/GPS_project/GPS/src
-gcc code_file.c -o trame_gps -lm
-./trame_gps
+make
+./script_trajectoire 2 /dev/ttyUSB0 chateau
